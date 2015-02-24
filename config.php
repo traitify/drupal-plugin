4cc5a45706f205824e3661b3bb435b34e1625411
Carson Wright <carsonnwright@gmail.com>
2015-02-24 15:33:18 -0500

    added config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/******************************************************************
+ * Requires Batteries!!!
+ * Get Your Public and Secret Key at https://developer.traitify.com
+ ******************************************************************/
+$TraitifyConfig = array(
+  'secret_key'=>"Your Secret Key",
+  'public_key'=>"Your Public Key",
+  'deck_id' => "career-deck"
+);
